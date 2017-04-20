0781e36cabd858eef54cbda568ea8d45ea19f3dd
Logan Bernard <bernard.logan4@gmail.com>
2017-04-20 11:45:22 -0400

    add redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { browserHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
+import { Provider } from 'react-redux'
 
+import configureStore from './store'
 import Routes from './routes'
 import './index.css'
 
+const store = configureStore()
+const history = syncHistoryWithStore(browserHistory, store)
+
 ReactDOM.render(
-  <Routes history={browserHistory} />,
+  <Provider store={store}>
+    <Routes history={history} />
+  </Provider>,
   document.getElementById('root')
 )
